b41688868b5da5ead1c0184d5ed7a7402048637e
remi-f930 <ficanoremi@gmail.com>
2026-07-25 01:18:57 +0200

    verif mail fix

--- js/auth.js
+++ js/auth.js
@@ -6,14 +6,20 @@ const login_button = document.querySelector("#login_button");
 const register_button = document.querySelector("#register_button");
 
 async function login(email, password) {
-    try {
-        await signInWithEmailAndPassword(auth, email, password);
-        status_message.style.color = "green";
-        status_message.textContent = "Welcome back";
-        window.location.href = "html/home.html";
-    } catch (err) {
+    if (!user.emailVerified) {
+        await sendEmailVerification(auth.currentUser);
         status_message.style.color = "red";
-        status_message.textContent = err.code;
+        status_message.textContent = `You must verify your email to continue, verification mail sent to ${email}`;
+    } else {
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            status_message.style.color = "green";
+            status_message.textContent = "Welcome back";
+            window.location.href = "html/home.html";
+        } catch (err) {
+            status_message.style.color = "red";
+            status_message.textContent = err.code;
+        }
     }
 }
 
